d5d9e7d3325ea7df9122ad47ecce5f40fcace895
AkarshOjha <akarahojha2004@gmail.com>
2024-10-14 15:37:00 +0530

    Update Register.jsx

--- Frontend/src/components/pages/Register.jsx
+++ Frontend/src/components/pages/Register.jsx
@@ -1,1 +1,12 @@
+import React, { useContext, useState } from 'react'
+import { Context } from '../../main'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import toast from 'react-hot-toast' 
 
+const Register = () => {
+  return(
+    <></>
+  );
+}
+export default Register
